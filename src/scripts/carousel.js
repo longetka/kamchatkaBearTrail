425e151455641535b9bdfe33747d5e2c62e53ef0
longetka <longetka@gmail.com>
2021-03-23 12:19:47 +0700

    layout microedit

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -2,7 +2,8 @@ const carousel = document.querySelector('.carousel');
 const testisCarousel = document.querySelector('.testis-carousel');
 const flkty = new Flickity( carousel, {
 	wrapAround: true,
-	setGallerySize: false
+	setGallerySize: false,
+	autoPlay: 5000,
 });
 const flkty1 = new Flickity( testisCarousel, {
 	// wrapAround: true,
